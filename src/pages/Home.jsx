2b0176c9bb2a7d2a353b454138c608c5101bfc91
user1 <user01@em/com>
2025-05-25 10:37:54 +0300

    improve ui

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -25,11 +25,11 @@ function Home() {
     <div className="p-5 flex flex-col items-start gap-2">
       <h1>number 1 = 5</h1>
       <h1>number 2 = 10</h1>
-      <h1>results = {counter}</h1>
-      <button className="bg-blue-300 p-1 hover:bg-blue-700" onClick={() => addnum()}>add</button>
-      <button className="bg-blue-300 p-1 hover:bg-blue-700" onClick={() => subnum()}>sub</button>
-      <button className="bg-blue-300 p-1 hover:bg-blue-700" onClick={() => multNum()}>mult</button>
-      <button className="bg-blue-300 p-1 hover:bg-blue-700" onClick={() => divNum()}>div</button>
+      <h1>results = <span className="bg-green-400 px-10 py-1 rounded-xl">{counter}</span></h1>
+      <button className="bg-blue-300 p-1 w-50 rounded-xl hover:bg-blue-700" onClick={() => addnum()}>add</button>
+      <button className="bg-blue-300 p-1 w-50 rounded-xl hover:bg-blue-700" onClick={() => subnum()}>sub</button>
+      <button className="bg-blue-300 p-1 w-50 rounded-xl hover:bg-blue-700" onClick={() => multNum()}>mult</button>
+      <button className="bg-blue-300 p-1 w-50 rounded-xl hover:bg-blue-700" onClick={() => divNum()}>div</button>
     </div>
   )
 }
